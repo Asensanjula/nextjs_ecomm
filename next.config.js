1791a614a4158a0cb901bb1f34cefe7c02ac217d
Asen Sanjula <asensanjula@gmail.com>
2021-09-06 23:56:01 +0530

    bug: change base url for testing

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: false,
 
   env:{
-    "BASE_URL":"http://localhost:3000/",
+    "BASE_URL":"https://shopper-gamma.vercel.app/",
     "MONGODB_URL": "mongodb+srv://pappzi:sxhVix2WycD2b6I2@cluster0.s4ito.mongodb.net/shopperDb?retryWrites=true&w=majority",
     "ACCESS_TOKEN_SECRET":"5b=D<GxQ5GZSU4HM}#{w[d:vH4,AuT_yC`,c'[Zdy.YuNTSZv",
     "REFRESH_TOKEN_SECRET":"SeF:.W`Nqdk,kfU~ThvJeY>fbSzA<<39,y;*EYBUw!8asRs$s;2]kzqv[t.Z2FN9&)KR!SFMQs>bB",
