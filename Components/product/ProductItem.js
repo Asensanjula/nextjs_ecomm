8e902e31624081ec0c6de1c554d77d312b55261d
Asen Sanjula <asensanjula@gmail.com>
2021-09-06 22:24:33 +0530

    feat: add product item and home page

---
+++ Components/product/ProductItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Button, Card} from "react-bootstrap";
+import Link from "next/link";
+import Row from "react-bootstrap/Row";
+
+function ProductItem({product}) {
+
+    const userLink = () => {
+        return (
+            <div className="d-flex justify-content-between">
+                <Link href={`product/${product._id}`}>
+                    <a className="btn btn-info mr-1 flex-fill">View</a>
+                </Link>
+                <Button variant="primary" className="ml-1 flex-fill">
+                    Buy
+                </Button>
+            </div>
+        )
+    };
+
+    return (
+        <Card className="productCard">
+            <Card.Img variant="top" src={product.images[0].url} />
+            <Card.Body className="d-flex flex-column">
+                <Card.Title className="text-capitalize">{product.title}</Card.Title>
+                <div className="d-flex justify-content-between">
+                    <h6 className="text-danger">${product.price}</h6>
+                    {
+                        product.inStock > 0 ?
+                            <h6 className="text-danger">In Stock: {product.inStock}</h6> :
+                            <h6 className="text-danger">Out Of Stock</h6>
+                    }
+
+                </div>
+                <Card.Text className="product__description text-justify flex-fill">{product.description}</Card.Text>
+                {userLink()}
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default ProductItem;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {getData} from "../Utility/fetchData";
+import Head from "next/head";
+import ProductItem from "../Components/product/ProductItem";
+import {Col, Row} from "react-bootstrap";
 
 const Home = ({productProps, result }) => {
     const [products, setProducts] = useState(productProps);
     return (
         <div>
-          Home
+         <Head>
+             <title>Home | The Shopper</title>
+         </Head>
+            <Row>
+            {
+                products.length === 0
+                ? <h2>No Products Found!</h2> :
+                    products.map((product)=> (
+                        <Col className="d-flex my-2" xs={12} sm={6} md={4} lg={3} key={product._id}>
+                            <ProductItem product={product}/>
+                        </Col>
+                    ))
+            }
+            </Row>
         </div>
     );
 };
